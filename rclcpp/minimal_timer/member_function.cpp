564c8908bbcc69d3f605fe706a09d52860b421b2
Carlos Orduno <carlos.a.orduno@intel.com>
2019-07-30 16:28:55 -0700

    Added a full example to test namespaces

--- rclcpp/minimal_timer/member_function.cpp
+++ rclcpp/minimal_timer/member_function.cpp
@@ -16,28 +16,64 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
- * member function as a callback from the timer. */
+class Helper
+{
+public:
+  explicit Helper(rclcpp::Node::SharedPtr node)
+  : node_{node}
+  {
+    publisher_ = node_->create_publisher<std_msgs::msg::String>("helper_topic", 10);
+
+    timer_ = node_->create_wall_timer(1s, std::bind(&Helper::timer_callback, this));
+  }
+
+private:
+  std::shared_ptr<rclcpp::Node> node_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  void timer_callback()
+  {
+    RCLCPP_INFO(node_->get_logger(),
+      "helper has namespace %s", node_->get_effective_namespace().c_str());
+
+    auto message = std_msgs::msg::String();
+    message.data = "this is helper";
+    publisher_->publish(message);
+  }
+};
 
 class MinimalTimer : public rclcpp::Node
 {
 public:
   MinimalTimer()
-  : Node("minimal_timer")
+  : Node("minimal_timer", "timer_ns")
   {
+    RCLCPP_INFO(this->get_logger(), "The namespace is %s", this->get_namespace());
+
+    sub_node_ = create_sub_node("helper_ns");
+
+    helper_ = std::make_unique<Helper>(sub_node_);
+
     timer_ = create_wall_timer(
       500ms, std::bind(&MinimalTimer::timer_callback, this));
   }
 
 private:
+  rclcpp::Node::SharedPtr sub_node_;
+  std::unique_ptr<Helper> helper_;
+
+  rclcpp::TimerBase::SharedPtr timer_;
+
   void timer_callback()
   {
-    RCLCPP_INFO(this->get_logger(), "Hello, world!");
+    RCLCPP_INFO(this->get_logger(), "minimal timer has namespace %s",
+    get_effective_namespace().c_str());
   }
-  rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char * argv[])
