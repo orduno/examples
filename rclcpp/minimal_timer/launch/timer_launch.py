866564c9b72b0dadb1c7d99e557bedc17a39e241
Carlos Orduno <carlos.a.orduno@intel.com>
2019-08-01 11:26:38 -0700

    Testing PushRosNamespace

--- rclcpp/minimal_timer/launch/timer_launch.py
+++ rclcpp/minimal_timer/launch/timer_launch.py
@@ -1,7 +1,7 @@
 """Launch a talker and a listener."""
 
 from launch import LaunchDescription
-# from launch_ros.actions import PushRosNamespace
+from launch_ros.actions import PushRosNamespace
 from launch.actions import GroupAction
 
 import launch.actions
@@ -9,23 +9,23 @@ import launch.substitutions
 import launch_ros.actions
 
 
-# def generate_launch_description():
-#     """Launch a talker and a listener."""
-#     return LaunchDescription([
-#         GroupAction([
-#             PushRosNamespace('launch_ns'),
-#             launch_ros.actions.Node(
-#                 package='examples_rclcpp_minimal_timer',
-#                 node_executable='timer_member_function',
-#                 output='screen')
-#         ])
-#     ])
-
 def generate_launch_description():
     """Launch a talker and a listener."""
     return LaunchDescription([
-        launch_ros.actions.Node(
-            package='examples_rclcpp_minimal_timer',
-            node_executable='timer_member_function',
-            output='screen')
+        GroupAction([
+            PushRosNamespace('launch_ns'),
+            launch_ros.actions.Node(
+                package='examples_rclcpp_minimal_timer',
+                node_executable='timer_member_function',
+                output='screen')
+        ])
     ])
+
+# def generate_launch_description():
+#     """Launch a talker and a listener."""
+#     return LaunchDescription([
+#         launch_ros.actions.Node(
+#             package='examples_rclcpp_minimal_timer',
+#             node_executable='timer_member_function',
+#             output='screen')
+#     ])
